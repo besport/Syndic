29378e2908016019b96bc75e7990b3e3d483cbff
Philippe Wang <philippe.wang@besport.com>
2015-12-17 15:05:20 +0100

    fix parsing for rfc822 dates with American timezones modified:   lib/syndic_date.ml

--- lib/syndic_date.ml
+++ lib/syndic_date.ml
@@ -26,23 +26,43 @@ let () =
      20 Mar 2013 03:47:14 +0000 *)
 let of_rfc822 s =
   let make_date day month year h m maybe_s z =
-    let month = if String.length month <= 3 then month
-		else String.sub month 0 3 in
+    let month =
+      if String.length month <= 3 then month
+      else String.sub month 0 3 in
     let month = Hashtbl.find month_to_int month in
     let date = Calendar.Date.make year month day in
     let s =
       if maybe_s <> "" && maybe_s.[0] = ':' then
-	float_of_string(String.sub maybe_s 1 (String.length maybe_s - 1))
+        float_of_string(String.sub maybe_s 1 (String.length maybe_s - 1))
       else 0. in
     let t = Calendar.Time.(make h m (Second.from_float s)) in
     if z = "" || z = "GMT" || z = "UT" then
       Calendar.(create date t)
     else
       (* FIXME: this should be made more robust. *)
-      let zh = sscanf (String.sub z 0 3) "%i" (fun i -> i)
-      and zm = sscanf (String.sub z 3 2) "%i" (fun i -> i) in
-      let tz = Calendar.Time.(Period.make (-zh) zm (Second.from_int 0)) in
-      Calendar.(create date (Time.add t tz))
+      let tz_offset =
+        match z with
+        | "EST" -> (-5) * 3600
+        | "EDT" -> (-4) * 3600
+        | "CST" -> (-6) * 3600
+        | "CDT" -> (-5) * 3600
+        | "MST" -> (-7) * 3600
+        | "MDT" -> (-6) * 3600
+        | "PST" -> (-8) * 3600
+        | "PDT" -> (-7) * 3600
+        | _ ->
+          let zh = sscanf (String.sub z 0 3) "%i" (fun i -> i) in
+          let zm = sscanf (String.sub z 3 2) "%i" (fun i -> i) in
+          let tz_sign = if zh < 0 then -1 else 1 in
+          if zh < 0 then
+            tz_sign * (((-zh) * 3600) + (zm * 60))
+          else tz_sign * ((zh * 3600) + (zm * 60))
+      in
+      let rt = Calendar.(create date t) in
+      (* XXX: We lose minutes with this conversion, but Calendar does not
+         propose to handle minutes. *)
+      let tz = Time_Zone.UTC_Plus (tz_offset / 3600) in
+      Calendar.convert rt tz Time_Zone.UTC
   in
   try
     if 'A' <= s.[0] && s.[0] <= 'Z' then (
@@ -70,10 +90,17 @@ let of_rfc3339 s =
     if z = "" || z.[0] = 'Z' then
       Calendar.(create date t)
     else
-      let tz =
+      let tz_offset =
         let open Calendar.Time in
-        sscanf z "%i:%i" (fun h m -> Period.make (-h) m (Second.from_int 0)) in
-      Calendar.(create date (Time.add t tz))
+        sscanf z "%i:%i" (fun h m ->
+	  let tz_sign = if h < 0 then -1 else 1 in
+	  if h < 0 then tz_sign * (((-h) * 3600) + (m * 60))
+	  else tz_sign * ((h * 3600) + (m * 60))) in
+      (* XXX: We lose minutes with this conversion, but Calendar does not
+         propose to handle minutes. *)
+      let tz = Time_Zone.UTC_Plus (tz_offset / 3600) in
+      let rt = Calendar.(create date t) in
+      Calendar.convert rt tz Time_Zone.UTC
   in
   (* Sometimes, the seconds have a decimal point
      See https://forge.ocamlcore.org/tracker/index.php?func=detail&aid=1414&group_id=83&atid=418 *)
